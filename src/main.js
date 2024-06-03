66bdafae2540a864a313215a68edfe340f75956e
JinjuSu <jinjuta.s.au@gmail.com>
2024-06-03 18:55:19 +1000

    Start project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import 'mdb-vue-ui-kit/css/mdb.min.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+createApp(App).use(router).mount("#app");
